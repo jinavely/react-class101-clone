0897418f145fcbb752ff67081f69a7923ea390e9
jinavely <gaia03060529@gmail.com>
2022-09-20 16:37:59 +0900

    404페이지 수정

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,20 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const NotFound = () => {
-    return (
-        <>  
-            <StyledNotFound>
-                <div>
-                    페이지를 찾을 수 없습니다
-                </div>
-            </StyledNotFound>
-        </>
-    );
+  return (
+    <>
+      <StyledNotFound>
+        <Info>
+          요청하신 페이지를 찾을 수 없습니다.
+          <br />
+          입력하신 주소가 정확한지 다시 한번 확인해 주세요.
+        </Info>
+        <Link to="/">홈으로 가기</Link>
+      </StyledNotFound>
+    </>
+  );
 };
 
 export default NotFound;
 
 const StyledNotFound = styled.div`
-    padding: 80px 0 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 1216px;
+  margin: 0 auto;
+  padding: 80px 0 60px;
+
+  a {
+    margin-top: 20px;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 3px;
+    color: rgb(26, 26, 26);
+    background-color: rgb(248, 248, 248);
+    font-weight: 500;
+    font-size: 14px;
+    letter-spacing: -0.2px;
+    padding: 0px 16px;
+    height: 40px;
+    transition: background-color 0.1s ease 0s;
+    text-decoration-line: none;
+  }
+`;
+
+const Info = styled.div`
+  text-align: center;
+  line-height: 1.5;
 `;
